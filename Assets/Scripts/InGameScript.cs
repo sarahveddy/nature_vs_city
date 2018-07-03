86b33fc72c4c4802ab2e842ddf4fc4c09c3e63de
Sarah Vedress <>
2018-07-03 11:31:35 -0600

    Started changing the environment materials

--- Assets/Scripts/InGameScript.cs
+++ Assets/Scripts/InGameScript.cs
@@ -142,7 +142,7 @@ public void setupDeathMenu (){
     public void processClicksPauseMenu ( MenuScript.PauseMenuEvents index  ){
         if (index == MenuScript.PauseMenuEvents.MainMenu )
 	{	
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene("TESTSCENE");
 		
             hMenuScript.InvokeShowMenu((int)MenuScript.MenuIDs.MainMenu);
 		iMenuStatus = 1;	//display main menu
@@ -163,12 +163,12 @@ public void setupDeathMenu (){
         if (index == MenuScript.GameOverMenuEvents.Play)
 	{
 		hMenuScript.showHUDElements();
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("TESTSCENE");
 		launchGame();
 	}
         else if (index == MenuScript.GameOverMenuEvents.Back)
 	{
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("TESTSCENE");
 		
             hMenuScript.InvokeShowMenu((int)MenuScript.MenuIDs.MainMenu);
 		iMenuStatus = 1;	//display main menu
