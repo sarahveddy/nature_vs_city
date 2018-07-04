e96b8d08f69b2f4151f4a7d293f7df7269751d17
Sarah Vedress <>
2018-07-04 14:43:56 -0600

    creating menus

--- Assets/Scripts/MainMenuButton.cs
+++ Assets/Scripts/MainMenuButton.cs
@@ -1,9 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenuButton : MonoBehaviour {
+public class MainMenuButton : MonoBehaviour
+{
 
+	public bool quit; 
+	public bool nature;
+	public bool city;
+
+	void OnMouseUp()
+	{
+		if (quit)
+		{
+			Debug.Log("Quit");
+			//Application.Quit();
+		}
+
+		if (nature)
+		{
+			Debug.Log("Nature");
+			//SceneManager.LoadScene("NatureScene");
+		}
+
+		if (city)
+		{
+			Debug.Log("City");
+			//SceneManager.LoadScene("CityScene");
+		}
+	}
+	
+	
 	// Use this for initialization
 	void Start () {
 		
