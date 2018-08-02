67ad39cd907a88dabb026bd2c5a1e568f92461a4
Sarah Vedress <>
2018-08-02 08:50:57 -0600

    Hide level select when random level is selected

--- Assets/Scripts/Starter.cs
+++ Assets/Scripts/Starter.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Starter : MonoBehaviour {
-
 	// Use this for initialization
+	private GameObject splash; 
 	void Start () {
+		splash = GameObject.Find("SplashScreen").gameObject; 
 		if (PersistentManagerScript.Instance.randomLevel)
 		{
 			int sceneIndex = Random.Range(1, 3);
 			SceneManager.LoadScene(sceneIndex);
 //			FindObjectOfType<Logging>().SendLog("SCENE SELECTED", sceneIndex.ToString());
+		}
+		else
+		{
+			splash.GetComponent<Canvas>().enabled = false; 
 		}
 	}
 	
